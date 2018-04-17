14a97884211155493ad777d26a380eee6aca5a82
saifiifazal <33841291+saifiifazal@users.noreply.github.com>
2018-04-18 00:29:12 +0500

    Update handlers.go

--- proxy/handlers.go
+++ proxy/handlers.go
@@ -5,8 +5,8 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/techievee/open-ethereum-pool/rpc"
-	"github.com/techievee/open-ethereum-pool/util"
+	"github.com/saifiifazal/open-ethereum-pool/rpc"
+	"github.com/saifiifazal/open-ethereum-pool/util"
 )
 
 // Allow only lowercase hexadecimal with 0x prefix
