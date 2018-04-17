9765b64e30f9594bb885702756287113e1604f7c
saifiifazal <33841291+saifiifazal@users.noreply.github.com>
2018-04-18 00:27:38 +0500

    Update payer.go

--- payouts/payer.go
+++ payouts/payer.go
@@ -10,9 +10,9 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
-	"github.com/techievee/open-ethereum-pool/rpc"
-	"github.com/techievee/open-ethereum-pool/storage"
-	"github.com/techievee/open-ethereum-pool/util"
+	"github.com/saifiifazal/open-ethereum-pool/rpc"
+	"github.com/saifiifazal/open-ethereum-pool/storage"
+	"github.com/saifiifazal/open-ethereum-pool/util"
 )
 
 const txCheckInterval = 5 * time.Second
